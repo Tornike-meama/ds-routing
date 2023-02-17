cef452edc943c30ede5e291344babb7aecc6b048
Tornike Bardanashvili <toko7bardanashvili@gmail.com>
2023-02-17 23:24:27 +0400

    first commit

---
+++ src/hooks/usePermission.hook.tsx
@@ -0,0 +1,33 @@
+import { createContext, useContext } from "react";
+import {
+  PermissionContextType,
+  UserPermissionReturnType,
+  Actions,
+  Acceseses,
+} from "types";
+export const PermissionsContext = createContext<PermissionContextType>({
+  userClaims: [],
+});
+
+function usePersmissions(
+  moduleKey?: string,
+  pageKeys?: Acceseses
+): UserPermissionReturnType {
+  const { userClaims } = useContext(PermissionsContext);
+  let actions = {} as Actions;
+
+  if (!pageKeys || !moduleKey) {
+    return { actions, userClaims };
+  }
+
+  const hasPageAccess = userClaims.some(
+    (key: string) => key === moduleKey || key === pageKeys.pageKey
+  );
+  Object.getOwnPropertyNames(pageKeys).forEach(
+    (key) =>
+      (actions[key] = hasPageAccess || userClaims.includes(pageKeys[key]))
+  );
+  return { actions, userClaims };
+}
+
+export default usePersmissions;
