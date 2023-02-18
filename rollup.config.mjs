244ba3945b142d63e2a35a07cb07558b20ea9803
Tornike Bardanashvili <toko7bardanashvili@gmail.com>
2023-02-18 11:33:06 +0400

    sdsa

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,8 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import packageJson from "./package.json" assert { type: "json" };
-import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import { terser } from "rollup-plugin-terser";
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 export default [
   {
@@ -22,11 +22,11 @@ export default [
       },
     ],
     plugins: [
-      peerDepsExternal(),
+      // peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser(),
+      // terser(),
     ],
   },
   {
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export * from "./components";
 export * from "./helpers";
 export * from "./hooks";
 export * from "./types";
+
+    // "rollup-plugin-dts": "^5.2.0",
+    // "rollup-plugin-peer-deps-external": "^2.2.4",
+    // "rollup-plugin-terser": "^7.0.2",
