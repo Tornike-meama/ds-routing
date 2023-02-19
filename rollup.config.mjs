62493e1145aa02584317cc5660a2b3d11a6fab2f
Tornike Bardanashvili <toko7bardanashvili@gmail.com>
2023-02-19 20:41:32 +0400

    a

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -34,7 +34,7 @@ export default [
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
+    input: "dist/cjs/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
   },
